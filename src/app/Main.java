7b3ca78141c2e31cd2dcffb2e276352d20c1b3b7
frolov-maks <ffrolov.maks@gmail.com>
2026-04-13 15:10:02 +0300

    Converter final version

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,22 +2,24 @@ package app;
 
 public class Main {
 
+    private static byte koef = 32;
+
     public static void main (String[] args) {
         System.out.println("Converter app v. 3.0");
 
-        double far = 53.5;
+        double far = 74.5;
         double cel = 36.6;
 
-        double F = CONV_TO_C(far);
-        double C = CONV_TO_F(cel);
+        double F = converteFarToCel(far);
+        double C = convertCelToFar(cel);
 
         System.out.printf("Result is %.2f Celsius degrees%n", F);
         System.out.printf("Result is %.2f Fahrenheit degrees%n", C);
     }
-    private static double CONV_TO_C(double far) {
-        return (far - 32) * 5/9;
+    private static double converteFarToCel(double far) {
+        return (far - koef) * 5/9;
     }
-    private static double CONV_TO_F(double cel) {
-        return (cel * 5/9) + 32;
+    private static double convertCelToFar(double cel) {
+        return (cel * 9/5) + koef;
     }
 }
