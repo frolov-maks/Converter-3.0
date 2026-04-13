9db1bbfa3b315d39ba1a53417fb83b4899b84417
frolov-maks <ffrolov.maks@gmail.com>
2026-04-13 13:58:41 +0300

    Celsius to Fahrenheit converter added

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,13 +5,19 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("Converter app v. 3.0");
 
-        double far = 53;
+        double far = 53.5;
+        double cel = 36.6;
 
         double F = CONV_TO_C(far);
+        double C = CONV_TO_F(cel);
 
         System.out.printf("Result is %.2f Celsius degrees%n", F);
+        System.out.printf("Result is %.2f Fahrenheit degrees%n", C);
     }
     private static double CONV_TO_C(double far) {
         return (far - 32) * 5/9;
+    }
+    private static double CONV_TO_F(double cel) {
+        return (cel * 5/9) + 32;
     }
 }
