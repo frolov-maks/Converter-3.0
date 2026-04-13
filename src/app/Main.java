3ff286e5326e4d422c9b833c43aba00eb55b6a92
frolov-maks <ffrolov.maks@gmail.com>
2026-04-13 13:31:03 +0300

    Start commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,7 @@
 package app;
 
 public class Main {
-    private static void main (String[] args) {
+    public static void main (String[] args) {
         System.out.println("Converter app v. 3.0");
     }
 }
